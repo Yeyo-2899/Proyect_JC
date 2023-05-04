b9754a178d805dcb36a1ff30cab3c18ddc3a7ec3
Yeyo-2899 <71796526+Yeyo-2899@users.noreply.github.com>
2023-05-04 01:37:04 -0600

    Update
    Correción de la posición inicial de los elementos serach-bar, filters y user-box.
    
    siguen habiendo problemas con el código Java Script

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,4 +1,4 @@
-//Este archivo contiene el codigo css necesario para controlar los eventos realcionados a el header
+//Este archivo contiene el codigo js necesario para controlar los eventos realcionados a el header
 let navbar = document.querySelector('.nav-items');
 let searchForm = document.querySelector('.search-form');
 let filters =  document.querySelector('.filter-box');
@@ -6,6 +6,8 @@ let useroptions =  document.querySelector('.user-box');
 let modal = document.querySelector('.modal');
 let header = document.querySelector('header');
 let headerModal = document.querySelector('.modal-header');
+
+console.log('Espacio de prueba');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -34,14 +36,10 @@ document.querySelector('#user-dropdown').onclick = () =>{
     searchForm.classList.remove('active');
 }
 
-/*document.querySelector('#modal-btn').onclick = () =>{
-    modal.classList.toggle('active');
-    headerModal.classList.toggle('active');
-    useroptions.classList.remove('active');
-    navbar.classList.remove('active');
-    filters.classList.remove('active');
-    searchForm.classList.remove('active');
-}*/
+document.querySelector('#explore-btn').onclick = () =>{
+    window.location.href = './html/recipe-book.html';
+}
+
 
 document.querySelector('#skip-btn').onclick = () =>{
     modal.classList.remove('active');
@@ -57,14 +55,23 @@ document.querySelector('#search-btn').onclick = () =>{
     filters.classList.remove('active');
 }
 
-/*document.querySelector('.nav-items').onclick = () =>{
+document.querySelector('.nav-items').onclick = () =>{
     navbar.classList.remove('active');
+}
+
+/*document.querySelector('#modal-btn').onclick = () =>{
+    modal.classList.toggle('active');
+    headerModal.classList.toggle('active');
+    useroptions.classList.remove('active');
+    navbar.classList.remove('active');
+    filters.classList.remove('active');
+    searchForm.classList.remove('active');
 }*/
 
-document.querySelector('#home-btn').onclick = () =>{
+/*document.querySelector('#home-btn').onclick = () =>{ //Cambiar este
     window.location.href = '../index.html';
-}
+}*/
 
-document.querySelector('#recipe-book-btn').onclick = () =>{
+/*document.querySelector('#recipe-book-btn').onclick = () =>{
     window.location.href = '../html/recipe-book.html';
-}
+}*/
