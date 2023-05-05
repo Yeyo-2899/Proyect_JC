0eafa84f7a3855eed5671ff1aac5e33c17e083f1
Yeyo-2899 <71796526+Yeyo-2899@users.noreply.github.com>
2023-05-05 13:10:06 -0600

    Horas de Atención
    En este comit se encuentras los cambios realizados en el código en horas de atención

--- dist/js/header.js
+++ dist/js/header.js
@@ -7,71 +7,77 @@ let modal = document.querySelector('.modal');
 let header = document.querySelector('header');
 let headerModal = document.querySelector('.modal-header');
 
-console.log('Espacio de prueba');
+document.addEventListener("DOMContentLoaded", () => {
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
+    window.addEventListener('scroll', function(){
+        let value = window.scrollY;
+        
+        header.style.marginTop = value * -0.5 + 'px';
+    })
+
+    document.querySelector('#menu-btn').onclick = () =>{
+        navbar.classList.toggle('active');
+        searchForm.classList.remove('active');
+        filters.classList.remove('active');
+        useroptions.classList.remove('active');
+    }
     
-    header.style.marginTop = value * -0.5 + 'px';
-})
+    document.querySelector('#search-dropdown').onclick = () =>{
+        searchForm.classList.toggle('active');
+        navbar.classList.remove('active');
+        filters.classList.remove('active');
+        useroptions.classList.remove('active');
+    }
+    
+    document.querySelector('#user-dropdown').onclick = () =>{
+        useroptions.classList.toggle('active');
+        navbar.classList.remove('active');
+        filters.classList.remove('active');
+        searchForm.classList.remove('active');
+    }
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    filters.classList.remove('active');
-    useroptions.classList.remove('active');
-}
+    let explore = document.querySelector('#explore-btn');
+    console.log(explore);
+    explore.onclick = () =>{
+        window.location.href = './html/recipe-book.html';
+    }
+    
+    
+    document.querySelector('#skip-btn').onclick = () =>{
+        modal.classList.remove('active');
+        headerModal.classList.remove('active');
+    }
+    
+    document.querySelector('#filter-btn').onclick = () =>{
+        filters.classList.toggle('active');
+        /*navbar.classList.remove('active');*/
+    }
+    
+    document.querySelector('#search-btn').onclick = () =>{
+        filters.classList.remove('active');
+    }
+    
+    document.querySelector('.nav-items').onclick = () =>{
+        navbar.classList.remove('active');
+    }
+    
+    document.querySelector('#modal-btn').onclick = () =>{
+        modal.classList.toggle('active');
+        headerModal.classList.toggle('active');
+        useroptions.classList.remove('active');
+        navbar.classList.remove('active');
+        filters.classList.remove('active');
+        searchForm.classList.remove('active');
+    }
+    
+    /*document.querySelector('#home-btn').onclick = () =>{ //Cambiar este
+        window.location.href = '../index.html';
+    }*/
+    
+    document.querySelector('#recipe-book-btn').onclick = () =>{
+        window.location.href = '../html/recipe-book.html';
+    }
+});
 
-document.querySelector('#search-dropdown').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    filters.classList.remove('active');
-    useroptions.classList.remove('active');
-}
-
-document.querySelector('#user-dropdown').onclick = () =>{
-    useroptions.classList.toggle('active');
-    navbar.classList.remove('active');
-    filters.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-document.querySelector('#explore-btn').onclick = () =>{
-    window.location.href = './html/recipe-book.html';
-}
 
 
-document.querySelector('#skip-btn').onclick = () =>{
-    modal.classList.remove('active');
-    headerModal.classList.remove('active');
-}
-
-document.querySelector('#filter-btn').onclick = () =>{
-    filters.classList.toggle('active');
-    /*navbar.classList.remove('active');*/
-}
-
-document.querySelector('#search-btn').onclick = () =>{
-    filters.classList.remove('active');
-}
-
-document.querySelector('.nav-items').onclick = () =>{
-    navbar.classList.remove('active');
-}
-
-/*document.querySelector('#modal-btn').onclick = () =>{
-    modal.classList.toggle('active');
-    headerModal.classList.toggle('active');
-    useroptions.classList.remove('active');
-    navbar.classList.remove('active');
-    filters.classList.remove('active');
-    searchForm.classList.remove('active');
-}*/
-
-/*document.querySelector('#home-btn').onclick = () =>{ //Cambiar este
-    window.location.href = '../index.html';
-}*/
-
-/*document.querySelector('#recipe-book-btn').onclick = () =>{
-    window.location.href = '../html/recipe-book.html';
-}*/
